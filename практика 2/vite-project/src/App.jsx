2457c6219fd44098b3c927a903a66eb5f3b64c3e
Vasilev Kharyskhan <prostochel.pochtachela@gmail.com>
2025-05-22 18:17:37 +0900

    Лабораторная 4 - стилизовать Footer Header

--- практика 2/vite-project/src/App.jsx
+++ практика 2/vite-project/src/App.jsx
@@ -1,22 +1,19 @@
-import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
-import Header from './componets/Header'
-import Footer from './componets/Footer'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import Blog from './pages/Blog'
 
 function App() {
 
   return (
     <>
-      <div className='bg-red-500 text-2xl'>
-        <Header />
-      </div>
-      <div className='bg-green-500 text-xl'>
+      <Header />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home/>} />
@@ -24,11 +21,8 @@ function App() {
           <Route path="/blog" element={<Blog/>} />
         </Routes>
       </BrowserRouter>
-      </div>
       
-      <div className='bg-blue-500 text-lg'>
       <Footer />
-      </div>
       
     </>
   )
--- практика 2/vite-project/src/pages/Home.tsx
+++ практика 2/vite-project/src/pages/Home.tsx
@@ -2,12 +2,16 @@ import React, { PureComponent, ReactNode } from 'react'
 
 interface Props {}
 interface State {}
-
 class Home extends PureComponent<Props, State> {
 
     render(): ReactNode {
         return (
-            <h1>Home</h1>
+            <>  
+                <div className="w-full flex">
+                    Home
+                </div>
+            </>
+            
         )
     }
 }
